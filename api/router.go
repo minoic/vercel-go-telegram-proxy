6737e8f013fb3b1327b8d0e4f2a0474766ac0f25
yiming.lu <yiming.lu@keboda.com>
2023-11-15 17:26:51 +0800

    init

---
+++ api/router.go
@@ -0,0 +1,17 @@
+package api
+
+import "net/http"
+import "github.com/gin-gonic/gin"
+
+var router *gin.Engine
+
+func init() {
+	router := gin.Default()
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello world!")
+	})
+}
+
+func Listen(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
+}
