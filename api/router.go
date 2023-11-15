6ebd3a0d5412d4cf4b757bb5b3987cfb4c6908bd
minoic <minoic2020@gmail.com>
2023-11-15 20:54:23 +0800

    add telegram proxy

--- api/router.go
+++ api/router.go
@@ -1,14 +1,36 @@
 package api
 
-import "net/http"
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 var router *gin.Engine
 
+var apiUrl = "https://api.telegram.org"
+
 func init() {
 	router = gin.Default()
-	router.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "Hello world!")
+	router.Any("/*path", func(context *gin.Context) {
+		uri := context.Param("path")
+		url := apiUrl + uri
+		req, err := http.NewRequestWithContext(context, context.Request.Method, url, context.Request.Body)
+		if err != nil {
+			fmt.Println(err)
+			context.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		req.Header = context.Request.Header
+		req.PostForm = context.Request.PostForm
+		req.Form = context.Request.Form
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			context.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		context.DataFromReader(resp.StatusCode, resp.ContentLength, "application/json", resp.Body, nil)
 	})
 }
 
